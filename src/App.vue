bbf1c3d7f649befd5061ac006fec0511bd099567
henrique.belotto <henrique.belotto@mcap.com>
2024-04-23 23:28:37 -0400

    minor fix in the app.vue

--- src/App.vue
+++ src/App.vue
@@ -32,19 +32,14 @@ const items = ref([
           <a href="/">
             <img
               src="/to-do-list-logo.png"
-              alt="Robot Factory"
+              alt="Todo List"
               class="border-round mr-2"
               style="max-height: 50px"
             />
           </a>
         </template>
         <template #item="{ item, props }">
-          <router-link
-            v-if="item.route"
-            v-slot="{ href, navigate }"
-            :to="item.route"
-            custom
-          >
+          <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
             <a v-ripple :href="href" v-bind="props.action" @click="navigate">
               <span :class="item.icon" />
               <span class="ml-2">{{ item.label }}</span>
